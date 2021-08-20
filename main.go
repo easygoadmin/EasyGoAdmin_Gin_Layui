c0896c87a970718c6954931eea250bfb6680c90d
半城风雨 <1260211623@qq.com>
2021-08-20 13:19:09 +0800

    签入版本

---
+++ main.go
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author 摆渡人
+ * @since 2021/8/20
+ * @File : main
+ */
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
